6f65610b7da4df226105eb2840b5dc59a00898b7
Marco Ávila <avila.marco.dev@gmail.com>
2023-07-12 18:19:30 +0100

    pages e libraries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import PostJob from "./pages/PostJob";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <div className="App">cenas</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hire-remotely" element={<PostJob />} />
+          <Route path="/sign-in" element={<PostJob />} />
+          <Route path="/sign-up" element={<PostJob />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
